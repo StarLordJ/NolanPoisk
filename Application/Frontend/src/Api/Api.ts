3d7499f8fffbc38f47d21729d3bfabb2528793b8
Semyon <boyarnikov@skbkontur.ru>
2019-06-28 16:59:48 +0500

    new ApiClient

--- Application/Frontend/src/Api/Api.ts
+++ Application/Frontend/src/Api/Api.ts
@@ -1,131 +1,127 @@
 import { Movie } from "components/MovieItem/MovieItem";
 import { Review } from "components/MoviePage/ReviewItem"
-import axios, { AxiosResponse } from "axios";
+import axios, {AxiosRequestConfig } from "axios";
 import { User } from 'index';
 
-export async function getAllMovies(): Promise<Movie[]> {
-    try {
-        const response = await axios.get("/api/movies");
-        return response.data;
-    } catch (e) {
-        throw Error(e);
-    }
+interface QueryParams {
+    [key: string]: string;
 }
 
-export async function getMovieInfo(movie: string): Promise<Movie[]> {
-    try {
-        const response = await axios.get(`/api/movie?name=${encodeURIComponent(movie)}`);
-        return response.data;
-    } catch (e) {
-        throw Error(e);
-    }
+interface QueryOptions {
+    params?: QueryParams;
+    config?: AxiosRequestConfig;
 }
 
-export async function getMovieReviews(movie: string): Promise<Review[]> {
-    try {
-        const response = await axios.get(`/api/review?name=${encodeURIComponent(movie)}`);
-        return response.data;
-    } catch (e) {
-        throw Error(e);
-    }
-}
+export class ApiClient {
+    private _user: User;
 
-export async function getMovieRatingOfUser(movie: string, email: string): Promise<number> {
-    try {
-        const response = await axios.get(`/api/userRating?name=${encodeURIComponent(movie)}&email=${encodeURIComponent(email)}`);
-        return response.data;
-    } catch (e) {
-        throw Error(e);
-    }
-}
+    public getAllMovies = async (): Promise<Movie[]> => {
+        return await this.get<Movie[]>("/api/movies");
+    };
 
-export async function getMovieRating(movie: string): Promise<{ rating: number; count: number }> {
-    try {
-        const response = await axios.get(`api/movieRating?name=${encodeURIComponent(movie)}`);
-        return response.data;
-    } catch (e) {
-        throw Error(e);
+    public set user(user: User) {
+        this._user = user;
     }
-}
 
-export async function setUserRating(rate: number, movie: string, email: string): Promise<void> {
-    try {
-        await axios.post("/api/userRating/set", { rate, movie, email })
-    } catch (e) {
-        throw Error(e);
-    }
-}
+    public getMovieInfo = async (movie: string): Promise<Movie> => {
+        return await this.get<Movie>("/api/movies", { params: { name: movie }});
+    };
 
-export async function deleteUserRating(movie: string, email: string): Promise<void> {
-    try {
-        await axios.post("api/userRating/delete", { movie, email })
-    } catch (e) {
-        throw Error(e)
-    }
-}
+    public getMovieReviews = async (movie: string): Promise<Review[]> => {
+        return await this.get<Review[]>("/api/review", { params: { name: movie }});
+    };
 
-export async function sendMovieReview(user: User, movie: string, text: string): Promise<void> {
-    try {
-        await axios.post("/api/review/set", {
+    public getMovieRatingOfUser = async (movie: string, email: string): Promise<number> => {
+        return await this.get<number>("/api/userRating", { params: { name: movie, email }});
+    };
+
+    public getMovieRating = async (movie: string): Promise<{ rating: number; count: number }> => {
+        return await this.get<{ rating: number; count: number }>("/api/movieRating", { params: { name: movie }});
+    };
+
+    public setUserRating = async (mark: number, movie: string, email: string): Promise<void> => {
+        await this.post("/api/userRating/set", { rate: mark, movie, email });
+    };
+
+    public deleteUserRating = async (movie: string, email: string): Promise<void> => {
+        await this.post("/api/userRating/delete", { movie, email });
+    };
+
+    public sendMovieReview = async (user: User, movie: string, text: string): Promise<void> => {
+        await this.post("/api/review/set", {
             movie: movie,
             text: text,
             username: user.name,
             email: user.email
-        })
-    } catch (e) {
-        throw Error(e);
-    }
-}
-
-export async function updateMovieReview(id: number, text: string): Promise<void> {
-    try {
-        await axios.post("/api/review/update", { id, text });
-    } catch (e) {
-        throw Error(e);
-    }
-}
-
-export async function deleteReview(id: number): Promise<void> {
-    try {
-        await axios.post("/api/review/delete", { id })
-    } catch (e) {
-        throw Error(e);
-    }
-}
-
-export async function authorizeUser(email: string, password: string): Promise<void> {
-    try {
-        const response = await axios.post("/api/login", { email, password });
-        localStorage.setItem("JWT", response.data.token);
-    } catch (e) {
-        throw Error(e);
-    }
-}
-
-export async function registerUser(name: string, email: string, password: string): Promise<void> {
-    try {
-        await axios.post("/api/register", { name, email, password });
-    } catch (e) {
-        throw Error(e);
-    }
-}
-
-export async function checkUserIsLogin(token: string): Promise<{
-    name: string;
-    email: string;
-    privilege: boolean;
-}> {
-    try {
-        const response = await axios.get("/api/check", {
-            headers: { Authorization: `JWT ${token}` }
         });
+    };
 
-        return response.data;
-    } catch (e) {
-        throw Error(e);
-    }
+    public updateMovieReview = async (id: number, text: string): Promise<void> => {
+        await this.post("/api/review/update", { id, text });
+    };
+
+    public deleteReview = async (id: number): Promise<void> => {
+        await this.post("/api/review/delete", { id });
+    };
+
+    public authorizeUser = async (email: string, password: string): Promise<void> => {
+        const response = await this.post("/api/login", { email, password });
+        localStorage.setItem("JWT", response.data.token);
+    };
+
+    public registerUser = async (name: string, email: string, password: string): Promise<void> => {
+        await this.post("/api/register", { name, email, password });
+    };
+
+    public checkUserIsLogin = async (token: string): Promise<{
+        name: string;
+        email: string;
+        privilege: boolean;
+    }> => {
+        return  await this.get<{
+            name: string;
+            email: string;
+            privilege: boolean;
+        }>("/api/check", {
+            config: {
+                headers: { Authorization: `JWT ${token}` }
+            }
+        });
+    };
+
+    public logOutUser = (): void => {
+        localStorage.removeItem("JWT");
+    };
+
+    private async get<T>(url: string, options: QueryOptions = {}): Promise<T> {
+        try {
+            const response = await axios.get(this.buildQueryString(url, options.params), options.config);
+            return response.data;
+        } catch (e) {
+            throw Error(e);
+        }
+    };
+
+    private async post(url: string, data?: any): Promise<any> {
+        try {
+            return await axios.post(url, data);
+        } catch (e) {
+            throw Error(e);
+        }
 }
 
-export function logOutUser(): void {
-    localStorage.removeItem("JWT");
+    private buildQueryString(url: string, params?: QueryParams) {
+        let queryString = url;
+
+        if (params) {
+            queryString += "?";
+
+            for (const [name, value] of Object.entries(params)) {
+                const pair = name + encodeURIComponent(value) + "&";
+                queryString += pair;
+            }
+        }
+
+        return queryString;
+    }
 }
