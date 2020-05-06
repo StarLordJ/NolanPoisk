279185821b88f912cfc0a19d2d3ab67be7fc36be
Boyarnikov Semyon <iboyarnikoff@gmail.com>
2020-05-06 23:21:40 +0500

    kek

--- Application/Frontend/src/components/App/App.tsx
+++ Application/Frontend/src/components/App/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { MoviesList } from "../MoviesList";
-import { Header } from "../Header"
+import { Header } from "../Header";
 import { ToastsContainer } from "../Toast/Toast";
 import { Router, Route } from "react-router";
-import { createHashHistory } from "history";
-import { MoviePage } from '../MoviePage';
+import { MoviePage } from "../MoviePage";
 import { User } from "../Types";
 
 export interface Props {
@@ -19,13 +18,13 @@ export class App extends React.Component<Props> {
 
     render() {
         return (
-            <Router history={createHashHistory()}>
+            <Router>
                 <ToastsContainer />
                 <Header />
                 <Route exact path="/" component={MoviesList} />
                 <Route path="/movie/:name" component={MoviePage} />
             </Router>
-        )
+        );
     }
 
     private checkIsLogin = (): void => {
@@ -36,5 +35,5 @@ export class App extends React.Component<Props> {
         } else {
             this.props.checkUserIsLogin(accessString);
         }
-    }
+    };
 }
