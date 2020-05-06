3da897db88352d8caea42e2824c58f52183d19c4
Boyarnikov Semyon <iboyarnikoff@gmail.com>
2020-05-06 22:16:32 +0500

    feat: add goback button

--- Application/Frontend/src/components/MoviePage/MoviePage.tsx
+++ Application/Frontend/src/components/MoviePage/MoviePage.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import { Reviews } from "./Reviews";
 import { ReviewForm } from "./ReviewForm";
-import { Rating } from './Rating';
-import { MoviePageInfo, User } from 'components/Types';
+import { Rating } from "./Rating";
+import { MoviePageInfo, User } from "components/Types";
 
 import styles from "./style.less";
+import { RouterProps } from "react-router";
 
-export interface Props {
-    match: {
-        params: {
-            name: string;
-        };
-    },
+export interface Props extends RouterProps {
     movie?: MoviePageInfo;
     user: User | null;
     getMovieInfo: () => void;
@@ -24,7 +20,11 @@ export class MoviePage extends React.Component<Props> {
         }
     }
 
-    render() {
+    private handleGoBack = () => {
+        this.props.history.goBack();
+    };
+
+    render(): React.ReactNode {
         const { movie } = this.props;
 
         return movie && movie.description ? (
@@ -33,7 +33,9 @@ export class MoviePage extends React.Component<Props> {
                     <img src={movie.posterUrl} alt={movie.name} />
                     <div className={styles.movieInfo}>
                         <h1 style={{ marginBottom: "10px" }}>{movie.name}</h1>
-                        <div style={{ color: "#6c6c6c", marginBottom: "20px" }}>{movie.tagline}</div>
+                        <div style={{ color: "#6c6c6c", marginBottom: "20px" }}>
+                            {movie.tagline}
+                        </div>
                         <div className={styles.itemDescription}>Жанр</div>
                         <div>{movie.genre.join(", ")}</div>
                         <hr className={styles.hr} />
@@ -45,6 +47,12 @@ export class MoviePage extends React.Component<Props> {
                         <hr className={styles.hr} />
                         <div className={styles.itemDescription}>Описание</div>
                         <div>{movie.description}</div>
+                        <button
+                            className={styles.buttonBack}
+                            onClick={this.handleGoBack}
+                        >
+                            Вернуться к фильмам
+                        </button>
                     </div>
                 </div>
                 <div className={styles.reviewsContainer}>
@@ -57,20 +65,28 @@ export class MoviePage extends React.Component<Props> {
                     <iframe
                         width="1000"
                         height="600"
-                        src={`https://www.youtube.com/embed/${movie.trailerUrl.split("v=")[1]}`}
+                        src={`https://www.youtube.com/embed/${
+                            movie.trailerUrl.split("v=")[1]
+                        }`}
                         frameBorder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen>
-                    </iframe>
+                        allowFullScreen
+                    ></iframe>
                 </div>
                 <div className={styles.formContainer}>
-                    {this.props.user ? <ReviewForm movie={movie.name} /> : <div>Зарегистрируйтесь или войдите, чтобы оставить рецензию!</div>}
+                    {this.props.user ? (
+                        <ReviewForm movie={movie.name} />
+                    ) : (
+                        <div>
+                            Зарегистрируйтесь или войдите, чтобы оставить
+                            рецензию!
+                        </div>
+                    )}
                 </div>
                 <div className={styles.reviewsContainer}>
                     <Reviews movie={movie.name} />
                 </div>
             </React.Fragment>
-        ) : null
+        ) : null;
     }
 }
-
